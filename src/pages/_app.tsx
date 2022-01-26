f2ad8668ce7eeabfb51c41dfa0e9faf2f1cb92e1
Den4200 <dennis@dennispham.me>
2022-01-26 13:46:50 -0500

    Wrap application in `PollSchemaProvider`.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import { AppProps } from 'next/app';
 
+import { PollSchemaProvider } from '@/hooks/pollSchema';
+
 import '../styles/global.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <PollSchemaProvider>
+    <Component {...pageProps} />
+  </PollSchemaProvider>
 );
 
 export default MyApp;
