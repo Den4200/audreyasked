75c9b7c51c5777aca7c18764059da5030cccd750
Den4200 <dennis@dennispham.me>
2022-01-26 13:45:37 -0500

    Put poll types in separate module.

---
+++ src/utils/types.ts
@@ -0,0 +1,36 @@
+export interface ObjectID {
+  id: number;
+}
+
+export enum QuestionType {
+  Checkbox,
+  Radio,
+  Text,
+}
+
+export interface Answer extends ObjectID {
+  sectionID: number;
+  questionID: number;
+  value: string;
+}
+
+export interface Question extends ObjectID {
+  sectionID: number;
+  type: QuestionType;
+  question: string;
+  answers: Answer[];
+}
+
+export interface Section extends ObjectID {
+  questions: Question[];
+  title: string;
+  addQuestionType: keyof typeof QuestionType;
+}
+
+export interface PollSchema {
+  title: string;
+  sections: Section[];
+}
+
+export const newID = (objs: ObjectID[]) =>
+  objs.length !== 0 ? Math.max(...objs.map((obj) => obj.id)) + 1 : 0;
