3e9e4d340422bb992d58b5e3b1c31b8b1974d3fc
Den4200 <dennis@dennispham.me>
2022-01-28 23:11:01 -0500

    Add edit poll page.

---
+++ src/pages/polls/[id]/edit.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+
+import { useRouter } from 'next/router';
+
+import PollEditor from '@/components/PollEditor';
+import { usePollSchema } from '@/hooks/pollSchema';
+import axios from '@/lib/axios';
+
+const EditPoll = () => {
+  const router = useRouter();
+  const { pollSchema, setPollSchema } = usePollSchema();
+
+  useEffect(() => {
+    const applyPollSchema = async () => {
+      if (!router.query.id) {
+        return;
+      }
+
+      const { data } = await axios.get(`polls/${router.query.id}`);
+      setPollSchema(data.poll.schema);
+    };
+    applyPollSchema();
+  }, [router.query.id, setPollSchema]);
+
+  const onSubmit = async () =>
+    axios.put(`polls/${router.query.id}`, {
+      poll: { schema: pollSchema },
+    });
+
+  return <PollEditor submitText="Update poll!" onSubmit={onSubmit} />;
+};
+
+export default EditPoll;
