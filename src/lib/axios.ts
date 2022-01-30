9ddb84dec39e724e917bcd1ff6fa2a06da2fcedf
Den4200 <dennis@dennispham.me>
2022-01-30 14:56:33 -0500

    Ensure response exists before checking status in axios interceptor.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -9,7 +9,7 @@ const apiClient = axios.create({
 apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response.status === 401) {
+    if (error.response?.status === 401) {
       signIn('discord');
     }
     return Promise.reject(error);
