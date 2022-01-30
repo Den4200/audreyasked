3c8f826dd3e2849fc2dd97350f803be85a1735e3
Den4200 <dennis@dennispham.me>
2022-01-30 13:39:19 -0500

    Redirect to poll after creation.

--- src/pages/polls/new.tsx
+++ src/pages/polls/new.tsx
@@ -1,15 +1,21 @@
+import { useRouter } from 'next/router';
+
 import PollEditor from '@/components/PollEditor';
 import { usePollSchema } from '@/hooks/pollSchema';
 import Meta from '@/layout/Meta';
 import axios from '@/lib/axios';
 
 const NewPoll = () => {
+  const router = useRouter();
   const { pollSchema } = usePollSchema();
 
-  const onSubmit = async () =>
-    axios.post('polls', {
+  const onSubmit = async () => {
+    const { data } = await axios.post('polls', {
       poll: { schema: pollSchema },
     });
+
+    router.push(`/polls/${data.poll.id}`);
+  };
 
   return (
     <PollEditor
