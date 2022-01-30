ac5edbc78a11aa80b3bf6815cd40baf36015726c
Den4200 <dennis@dennispham.me>
2022-01-30 13:35:04 -0500

    Create `usePollResponse` hook.

---
+++ src/hooks/pollResponse.ts
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+
+import usePoll from '@/hooks/poll';
+import { PollResponse } from '@/utils/types';
+
+const usePollResponse = (pollId?: string) => {
+  const { poll, setPollId } = usePoll(pollId);
+  const [pollResponse, setPollResponse] = useState<PollResponse>({
+    sections: [],
+  });
+
+  useEffect(
+    () =>
+      setPollResponse({
+        sections: poll?.schema.sections.map((section) => ({
+          id: section.id,
+          questions: section.questions.map((question) => ({
+            id: question.id,
+            answers: [],
+          })),
+        })),
+      }),
+    [poll]
+  );
+
+  const setAnswer = (sectionId: number, questionId: number, values: string[]) =>
+    setPollResponse({
+      sections: pollResponse.sections.map((section) =>
+        section.id === sectionId
+          ? {
+              ...section,
+              questions: section.questions.map((question) =>
+                question.id === questionId
+                  ? {
+                      ...question,
+                      answers: values,
+                    }
+                  : question
+              ),
+            }
+          : section
+      ),
+    });
+
+  const setCheckboxAnswer = (
+    sectionId: number,
+    questionId: number,
+    answerId: string
+  ) => {
+    const { answers } = pollResponse.sections
+      .find((section) => section.id === sectionId)!
+      .questions.find((question) => question.id === questionId)!;
+
+    if (answers.includes(answerId)) {
+      setAnswer(
+        sectionId,
+        questionId,
+        answers.filter((answer) => answer !== answerId)
+      );
+    } else {
+      setAnswer(sectionId, questionId, [...answers, answerId]);
+    }
+  };
+
+  return {
+    poll,
+    setPollId,
+    pollResponse,
+    setAnswer,
+    setCheckboxAnswer,
+  };
+};
+
+export default usePollResponse;
