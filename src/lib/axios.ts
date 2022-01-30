ef60489c19a4154db64f82d6218a6128e7dacfd6
Den4200 <dennis@dennispham.me>
2022-01-29 22:30:41 -0500

    Automagically redirect to sign in on `401` status from API.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,19 @@
 import axios from 'axios';
+import { signIn } from 'next-auth/react';
 
-export default axios.create({
+const apiClient = axios.create({
   baseURL: '/api/',
   withCredentials: true,
 });
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      signIn('discord');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
