9e2f6802de94be346d0e8877f9ee8d0ead7886df
Den4200 <dennis@dennispham.me>
2022-01-30 13:33:46 -0500

    Name groups used in `Checkbox` and `RadioButton` componenets.

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -8,7 +8,7 @@ const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
   ({ className, ...rest }, ref) => (
     <label
       className={clsxm(
-        'group cursor-pointer block h-4 w-4 mr-2 relative select-none',
+        'group-option cursor-pointer block h-4 w-4 mr-2 relative select-none',
         className
       )}
     >
@@ -22,7 +22,7 @@ const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
       <span
         className={clsxm(
           'bg-white border-2 border-pink-400 rounded h-6 w-6 absolute left-0 top-0 transition-colors duration-200 ease-linear',
-          'group-hover:bg-pink-200 peer-checked:bg-pink-300'
+          'group-option-hover:bg-pink-200 peer-checked:bg-pink-300'
         )}
       />
       <span className="absolute hidden peer-checked:block top-[6px] left-1 transition-colors duration-300 ease-linear">
