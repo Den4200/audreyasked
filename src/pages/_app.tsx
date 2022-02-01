b83470a3362881d1997d4bf97d04dead8ed13c44
Den4200 <dennis@dennispham.me>
2022-01-31 19:39:08 -0500

    Add loading screen.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,49 @@
-import { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
 
+import { SessionProvider } from 'next-auth/react';
+import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { HashLoader } from 'react-spinners';
+
+import '@/styles/global.css';
 import { PollSchemaProvider } from '@/hooks/pollSchema';
 
-import '../styles/global.css';
+const MyApp = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) => {
+  const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(false);
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <PollSchemaProvider>
-    <Component {...pageProps} />
-  </PollSchemaProvider>
-);
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      if (url !== router.asPath) {
+        setLoading(true);
+      } else {
+        setLoading(false);
+      }
+    };
+    const handleComplete = () => {
+      setLoading(false);
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+  }, [router]);
+
+  return (
+    <SessionProvider session={session}>
+      <PollSchemaProvider>
+        {loading ? (
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <HashLoader color="#F472B6" />
+          </div>
+        ) : null}
+        <Component {...pageProps} />
+      </PollSchemaProvider>
+    </SessionProvider>
+  );
+};
 
 export default MyApp;
