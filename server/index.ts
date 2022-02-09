278714b9c3000d30cf27cbfc25c38eaf79d1a9ef
Den4200 <dennis@dennispham.me>
2022-02-09 00:21:07 -0500

    Add websocket broadcast functionality for live poll responses.

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,9 @@ import url from 'url';
 import next from 'next';
 import WebSocket from 'ws';
 
+const pollResponsesRe = /\/polls\/([a-z0-9]+)\/responses\/?/;
+const pollRe = /\/polls\/([a-z0-9]+)\/?/;
+
 const dev = process.env.NODE_ENV !== 'production';
 const port = parseInt(process.env.PORT || '3000', 10);
 
@@ -11,23 +14,49 @@ const app = next({ dev, port });
 const handler = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const wss = new WebSocket.Server({ noServer: true });
   const server = createServer((req, res) =>
     handler(req, res, url.parse(req.url!, true))
   );
+  const wss = new WebSocket.Server({ noServer: true });
 
-  wss.on('connection', async (ws) => {
-    console.log('incoming connection');
+  const clientMap = new WeakMap<
+    WebSocket,
+    { pollId: string; sender: boolean }
+  >();
 
-    ws.on('close', () => {
-      console.log('connection closed');
+  wss.on('connection', async (ws, req) => {
+    const pollMatch = req.url!.match(pollRe);
+    const respMatch = req.url!.match(pollResponsesRe);
+
+    if (respMatch) {
+      clientMap.set(ws, { pollId: respMatch[1]!, sender: false });
+    } else if (pollMatch) {
+      clientMap.set(ws, { pollId: pollMatch[1]!, sender: true });
+    } else {
+      ws.close();
+    }
+
+    ws.on('message', (data) => {
+      wss.clients.forEach((client) => {
+        const clientData = clientMap.get(client);
+        const wsData = clientMap.get(ws);
+
+        if (
+          client !== ws &&
+          client.readyState === WebSocket.OPEN &&
+          !clientData?.sender &&
+          clientData?.pollId === wsData?.pollId
+        ) {
+          client.send(data);
+        }
+      });
     });
   });
 
   server.on('upgrade', (req, socket, head) => {
     const { pathname } = url.parse(req.url!, true);
 
-    if (!dev || pathname !== '/_next/webpack-hmr') {
+    if (pathname?.match(pollRe) || pathname?.match(pollResponsesRe)) {
       wss.handleUpgrade(req, socket, head, (ws) => {
         wss.emit('connection', ws, req);
       });
@@ -39,6 +68,7 @@ app.prepare().then(() => {
       throw err;
     }
 
-    console.log('server started');
+    // eslint-disable-next-line no-console
+    console.log(`> Server ready to serve requests (:${port})`);
   });
 });
