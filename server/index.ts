24d497089ac248594272ee73671a15ec749dda63
Den4200 <dennis@dennispham.me>
2022-02-08 21:12:56 -0500

    Add custom next server with websocket capabilities.

---
+++ server/index.ts
@@ -0,0 +1,44 @@
+import { createServer } from 'http';
+import url from 'url';
+
+import next from 'next';
+import WebSocket from 'ws';
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = parseInt(process.env.PORT || '3000', 10);
+
+const app = next({ dev, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const wss = new WebSocket.Server({ noServer: true });
+  const server = createServer((req, res) =>
+    handler(req, res, url.parse(req.url!, true))
+  );
+
+  wss.on('connection', async (ws) => {
+    console.log('incoming connection');
+
+    ws.onclose = () => {
+      console.log('connection closed');
+    };
+  });
+
+  server.on('upgrade', (req, socket, head) => {
+    const { pathname } = url.parse(req.url!, true);
+
+    if (!dev || pathname !== '/next/webpack-hmr') {
+      wss.handleUpgrade(req, socket, head, (ws) => {
+        wss.emit('connection', ws, req);
+      });
+    }
+  });
+
+  server.listen(port, (err?: any) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log('server started');
+  });
+});
