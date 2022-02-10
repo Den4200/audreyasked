39f07e13520939e7d0d24b05117faf9b07543dbc
Den4200 <dennis@dennispham.me>
2022-02-09 20:23:41 -0500

    Add prisma middleware to broadcast new poll responses to clients.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,27 @@
 import { PrismaClient } from '@prisma/client';
+import { WebSocket } from 'ws';
 
-export default new PrismaClient();
+import { parsePollResponse } from '@/utils/types';
+
+const prisma = new PrismaClient();
+
+prisma.$use(async (params, next) => {
+  const result = await next(params);
+
+  if (params.action === 'create' && params.model === 'PollResponse') {
+    const ws = new WebSocket(
+      `ws://127.0.0.1:${process.env.PORT || 3000}/api/ws?pollId=${
+        result.pollId
+      }&sender=true`
+    );
+
+    ws.on('open', () => {
+      ws.send(JSON.stringify({ pollResponse: parsePollResponse(result) }));
+      ws.close();
+    });
+  }
+
+  return result;
+});
+
+export default prisma;
