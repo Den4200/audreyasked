5b3d989a91bd1a5d1eebc0a9123d007b6355ae40
Den4200 <dennis@dennispham.me>
2022-02-13 17:32:05 -0500

    Catch errors in `usePoll` hook.

--- src/hooks/poll.ts
+++ src/hooks/poll.ts
@@ -3,7 +3,11 @@ import { useEffect, useState } from 'react';
 import axios from '@/lib/axios';
 import { Poll } from '@/utils/types';
 
+import { useErrorHandler } from './errorHandler';
+
 const usePoll = (id?: string) => {
+  const { setError } = useErrorHandler();
+
   const [pollId, setPollId] = useState<string | undefined>(id);
   const [poll, setPoll] = useState<Poll>({
     id: '',
@@ -15,16 +19,20 @@ const usePoll = (id?: string) => {
 
   useEffect(() => {
     const getPoll = async () => {
-      const { data } = await axios.get(`polls/${pollId}`);
-      data.poll.createdAt = new Date(data.poll.createdAt);
-      data.poll.updatedAt = new Date(data.poll.updatedAt);
-      setPoll(data.poll);
+      try {
+        const { data } = await axios.get(`polls/${pollId}`);
+        data.poll.createdAt = new Date(data.poll.createdAt);
+        data.poll.updatedAt = new Date(data.poll.updatedAt);
+        setPoll(data.poll);
+      } catch (err: any) {
+        setError(err);
+      }
     };
 
     if (pollId) {
       getPoll();
     }
-  }, [pollId, setPoll]);
+  }, [pollId, setPoll, setError]);
 
   return { poll, setPollId };
 };
