fea0a97327fd3b19b242dd624750fb775b26f7f9
Den4200 <dennis@dennispham.me>
2022-02-14 23:13:24 -0500

    Add `EMAIL_SERVER` and `EMAIL_FROM` env vars.

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,6 +3,8 @@ declare namespace NodeJS {
     DATABASE_URL: string;
     DISCORD_CLIENT_ID: string;
     DISCORD_CLIENT_SECRET: string;
+    EMAIL_SERVER?: string;
+    EMAIL_FROM?: string;
     NEXTAUTH_URL: string;
     SECRET: string;
     PORT: string;
