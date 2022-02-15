7d82baf0a603d43af89ef093b63ec33278d99efa
Den4200 <dennis@dennispham.me>
2022-02-14 23:15:28 -0500

    Add email provider to next auth.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import Discord from 'next-auth/providers/discord';
+import EmailProvider from 'next-auth/providers/email';
 
 import prisma from '@/lib/prisma';
 
@@ -11,6 +12,10 @@ const authHandler: NextApiHandler = (req, res) =>
       Discord({
         clientId: process.env.DISCORD_CLIENT_ID,
         clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      }),
+      EmailProvider({
+        server: process.env.EMAIL_SERVER,
+        from: process.env.EMAIL_FROM,
       }),
     ],
     adapter: PrismaAdapter(prisma),
