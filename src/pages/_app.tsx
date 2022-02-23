f6dcae76c99de4ee07d27f8ddee2995fabeaabba
Den4200 <dennis@dennispham.me>
2022-02-23 18:57:19 -0500

    Add DnD provider to app with `HTML5toTouch` backend.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,37 @@
 import { useEffect, useState } from 'react';
 
+import '@/styles/global.css';
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import {
+  DndProvider,
+  MouseTransition,
+  TouchTransition,
+} from 'react-dnd-multi-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 
-import '@/styles/global.css';
 import Loading from '@/components/Loading';
 import { ErrorHandlerProvider } from '@/hooks/errorHandler';
 import { PollSchemaProvider } from '@/hooks/pollSchema';
+
+const HTML5toTouch = {
+  backends: [
+    {
+      id: 'html5',
+      backend: HTML5Backend,
+      transition: MouseTransition,
+    },
+    {
+      id: 'touch',
+      backend: TouchBackend,
+      options: { enableMouseEvents: true },
+      preview: true,
+      transition: TouchTransition,
+    },
+  ],
+};
 
 const MyApp = ({
   Component,
@@ -34,14 +58,17 @@ const MyApp = ({
   }, [router]);
 
   return (
-    <SessionProvider session={session}>
-      <ErrorHandlerProvider>
-        <PollSchemaProvider>
-          {loading ? <Loading /> : null}
-          <Component {...pageProps} />
-        </PollSchemaProvider>
-      </ErrorHandlerProvider>
-    </SessionProvider>
+    // @ts-ignore
+    <DndProvider options={HTML5toTouch}>
+      <SessionProvider session={session}>
+        <ErrorHandlerProvider>
+          <PollSchemaProvider>
+            {loading ? <Loading /> : null}
+            <Component {...pageProps} />
+          </PollSchemaProvider>
+        </ErrorHandlerProvider>
+      </SessionProvider>
+    </DndProvider>
   );
 };
 
